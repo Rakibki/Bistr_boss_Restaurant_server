989151db2daf7ba6fbe2ec6f940226f133dabae7
Rakib mia <rbepari404@gmail.com>
2023-11-10 10:50:47 -0600

    basic server ready

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const express = require('express')
 const cors = require('cors')
+const port = process.env.PORT || 4500;
 
 const app = express()
 
+app.use(cors())
+app.use(express.json())
+
+
+app.get("/", (req, res) => {
+    res.send("bistro boss server")
+})
+
+
+app.listen(port, () => {
+    console.log(`bistro boss server is running port: ${port}`);
+})
