9eafd6d9463d52ce91875ee6dd6b5dcae4547773
Rakib mia <rbepari404@gmail.com>
2023-11-11 08:42:08 -0600

    create all nemu api

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const cors = require('cors')
 const port = process.env.PORT || 4500;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 
 const app = express()
 
@@ -11,6 +13,41 @@ app.use(express.json())
 app.get("/", (req, res) => {
     res.send("bistro boss server")
 })
+
+const uri = `mongodb+srv://DB_BISTRO_BOSS:AZNNAsUI9GZClBSc@cluster0.sinogwr.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("DB_bistro_boss");
+    const menuCollectopn = database.collection("menuCollectopn");
+    const reviewsCollection = database.collection("reviewsCollection");
+
+    app.get("/menu", async(req, res) => {
+      const result = await menuCollectopn.find().toArray();
+      res.send(result)
+    })
+
+    app.get("/reviews", async(req, res) => {
+      const result = await reviewsCollection.find().toArray();
+      res.send(result)
+    })
+
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 app.listen(port, () => {
